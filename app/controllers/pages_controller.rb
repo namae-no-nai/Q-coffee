a00455d2d0f6d1e0a2ce506f3bbf0cefd96eedb5
guilherme luiz <guilhermebluiz@hotmail.com>
2022-12-26 21:10:11 -0300

    pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,38 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!
+  before_action :set_coffee, only: [:roast, :sensory, :taste]
 
   def home
+    @user = current_user
+  end
+
+  def roast
+    @category_roast = Coffee.all.pluck(:roast).uniq
+    @selection = []
+    @category_roast.each do |roast|
+      @selection << Coffee.category_search(roast)
+    end
+  end
+
+  def sensory
+    @category_sensory = Coffee.all.pluck(:sensory).uniq
+    @selection = []
+    @category_sensory.each do |sensory|
+      @selection << Coffee.category_search(sensory)
+    end
+  end
+
+  def taste
+    @category_taste = Coffee.all.pluck(:taste).uniq
+    @selection = []
+    @category_taste.each do |taste|
+      @selection << Coffee.category_search(taste)
+    end
+  end
+
+  private
+
+  def set_coffee
+    @coffees = Coffee.all
   end
 end

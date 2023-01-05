b3e3a952ca2807723b5e67d5c7c824936a05d9c5
guilherme luiz <guilhermebluiz@hotmail.com>
2023-01-04 22:56:30 -0300

    placeholder search without use

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'category/:category_name', to: 'coffees#category', as: 'category'
-  get 'search', to: 'coffees#search'
   get 'ocr', to: 'photos#ocr'
   get 'photo_search_path/:pic', to: 'photos#photo_search', as: 'photo_search'
   post 'photos', to: 'photos#create', as: 'photos'
   get 'liked_coffees/:id', to: 'coffees#liked_coffees', as: 'liked_coffees'
-  get 'my_coffees/:id', to: 'coffees#my_coffees', as: 'my_coffees'
+  get 'my_coffees/:user_id', to: 'coffees#my_coffees', as: 'my_coffees'
   get 'my_reviews/:id', to: 'coffees#my_reviews', as: 'my_reviews'
   get 'filter/:filter_param', to: 'pages#filter', as: 'filter'
   get 'map', to: 'coffees#coffee_map', as: 'coffee_map'
